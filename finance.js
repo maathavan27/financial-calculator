859d13c9f624073b10712c9436077bf0f96cfb34
maathavan27 <33641294+maathavan27@users.noreply.github.com>
2021-06-11 20:26:00 -0400

    added js to radio buttons and made input boxes positive only

--- finance.js
+++ finance.js
@@ -1,1 +1,22 @@
-//js
+
+window.onload = function() {
+    document.getElementById("finances").reset();
+}
+
+function showQuestion(question) {
+    question.style.display = "block";
+}
+
+function hideQuestion(question) {
+    question.style.display = "none";
+}
+
+function checkForNegative(numBox, error) {
+    console.log(numBox.value)
+    var temp = parseFloat(numBox.value);
+    if (isNaN(temp) || temp < 0) {
+        error.style.display = "block";
+    } else {
+        error.style.display = "none";
+    }
+}
