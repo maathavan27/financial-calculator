2fd5b470802bdfba29783213c888421c0eab1546
maathavan27 <33641294+maathavan27@users.noreply.github.com>
2021-06-12 15:35:10 -0400

    restructured folder/added templates folder for flask

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, redirect, url_for, request, send_file
+import requests, json
+import time
+
+app = Flask(__name__)
+
+#when play is pressed
+@app.route("/", methods=['GET','POST'])
+def index():
+
+    return render_template('finance.html')
+
+@app.route("/notification.mp3")
+def notification():
+    return send_file('notification.mp3', attachment_filename='notification.mp3')
+
+if __name__ == '__main__':
+    app.run(debug=True)
