162f68a616bbb75f219fa78ed11d2df78a95c9ae
maathavan27 <33641294+maathavan27@users.noreply.github.com>
2021-06-12 21:49:01 -0400

    finished adapting questions, ready for table

--- static/js/finance.js
+++ static/js/finance.js
@@ -60,8 +60,7 @@ function inputSubs(numBox, error) {
         finances.appendChild(money);
         finances.appendChild(err);
         finances.appendChild(br);
-    }
-    
+    } 
 }
 
 function removeSubs() {
@@ -78,60 +77,6 @@ function removeSubs() {
     for (var i = 0; i < fullList.length; i++) {
         fullList[i].remove();
     }
-}
-
-function printValues() {
-    var subList = Array.prototype.slice.call(document.getElementsByClassName("subscriptions"), 0);
-    var moneyList = Array.prototype.slice.call(document.getElementsByClassName("subsMoney"), 0);
-    for (var i = 0; i < moneyList.length; i++) { //shows how to get values
-        console.log(subList[i].value + ": $" + moneyList[i].value);
-    }
-    console.log("abc");
-}
-
-var income, mortgage, rent, subList, moneyList, car, ins, phone, internet, loan, expenseList, expmoneyList;
-function store() {
-    income = document.getElementById("salary").value;
-    mortgage = document.getElementById("mortgageNum").value;
-    rent = document.getElementById("rentNum").value;
-    var tempSub = Array.prototype.slice.call(document.getElementsByClassName("subscriptions"), 0);
-    subList = [];
-    for (var i = 0; i < tempSub.length; i++) {
-        subList.push(tempSub[i].value);
-    }
-    var tempMoney = Array.prototype.slice.call(document.getElementsByClassName("subsMoney"), 0);
-    moneyList = [];
-    for (var i = 0; i < tempMoney.length; i++) {
-        moneyList.push(tempMoney[i].value);
-    }
-    car = document.getElementById("carnum").value;
-    ins = document.getElementById("insnum").value;
-    phone = document.getElementById("phonenum").value;
-    internet = document.getElementById("internetnum").value;
-    loan = document.getElementById(loannum);
-    var tempExpenses = Array.prototype.slice.call(document.getElementsByClassName("expenses"), 0);
-    expenseList = []
-    for (var i = 0; i < tempExpenses.length; i++) {
-        expenseList.push(tempExpenses[i].value);
-    }
-    var tempExpMoney = Array.prototype.slice.call(document.getElementsByClassName("expMoney"), 0);
-    expmoneyList = []
-    for (var i = 0; i < tempExpMoney.length; i++) {
-        expmoneyList.push(tempExpMoney[i].value);
-    }
-    localStorage.setItem('income', income);
-    localStorage.setItem('mortgage', mortgage);
-    localStorage.setItem('rent', rent);
-    localStorage.setItem('subList', JSON.stringify(subList));
-    localStorage.setItem('moneyList', JSON.stringify(moneyList));
-    localStorage.setItem('car', car);
-    localStorage.setItem('ins', ins);
-    localStorage.setItem('phone', phone);
-    localStorage.setItem('internet', internet);
-    localStorage.setItem('loan', loan);
-    localStorage.setItem('expenseList', JSON.stringify(expenseList));
-    localStorage.setItem('expMoneyList', JSON.stringify(expmoneyList));
-    console.log('stored');
 }
 
 function inputExp(numBox, error){
@@ -181,3 +126,58 @@ function removeExps() {
         fullList[i].remove();
     }
 }
+
+function printValues() {
+    var subList = Array.prototype.slice.call(document.getElementsByClassName("subscriptions"), 0);
+    var moneyList = Array.prototype.slice.call(document.getElementsByClassName("subsMoney"), 0);
+    for (var i = 0; i < moneyList.length; i++) { //shows how to get values
+        console.log(subList[i].value + ": $" + moneyList[i].value);
+    }
+    console.log("abc");
+}
+
+var income, mortgage, rent, subList, moneyList, car, ins, phone, internet, loan, expenseList, expmoneyList;
+function store() {
+    income = document.getElementById("salary").value;
+    mortgage = document.getElementById("mortgageNum").value;
+    rent = document.getElementById("rentNum").value;
+    var tempSub = Array.prototype.slice.call(document.getElementsByClassName("subscriptions"), 0);
+    subList = [];
+    for (var i = 0; i < tempSub.length; i++) {
+        subList.push(tempSub[i].value);
+    }
+    var tempMoney = Array.prototype.slice.call(document.getElementsByClassName("subsMoney"), 0);
+    moneyList = [];
+    for (var i = 0; i < tempMoney.length; i++) {
+        moneyList.push(tempMoney[i].value);
+    }
+    car = document.getElementById("carnum").value;
+    ins = document.getElementById("insnum").value;
+    phone = document.getElementById("phonenum").value;
+    internet = document.getElementById("internetnum").value;
+    loan = document.getElementById("loannum").value;
+    var tempExpenses = Array.prototype.slice.call(document.getElementsByClassName("expenses"), 0);
+    expenseList = []
+    for (var i = 0; i < tempExpenses.length; i++) {
+        expenseList.push(tempExpenses[i].value);
+    }
+    var tempExpMoney = Array.prototype.slice.call(document.getElementsByClassName("expMoney"), 0);
+    expmoneyList = []
+    for (var i = 0; i < tempExpMoney.length; i++) {
+        expmoneyList.push(tempExpMoney[i].value);
+    }
+    localStorage.setItem('income', income);
+    localStorage.setItem('mortgage', mortgage);
+    localStorage.setItem('rent', rent);
+    localStorage.setItem('subList', JSON.stringify(subList));
+    localStorage.setItem('moneyList', JSON.stringify(moneyList));
+    localStorage.setItem('car', car);
+    localStorage.setItem('ins', ins);
+    localStorage.setItem('phone', phone);
+    localStorage.setItem('internet', internet);
+    localStorage.setItem('loan', loan);
+    localStorage.setItem('expenseList', JSON.stringify(expenseList));
+    localStorage.setItem('expmoneyList', JSON.stringify(expmoneyList));
+    console.log('stored');
+}
+
--- static/js/results.js
+++ static/js/results.js
@@ -7,7 +7,6 @@ window.onload = function(){
     mortgage = localStorage.getItem('mortgage');
     rent = localStorage.getItem('rent');
     subList = JSON.parse(localStorage.getItem('subList'));
-    console.log(subList);
     moneyList = JSON.parse(localStorage.getItem('moneyList'));
     car = localStorage.getItem('car');
     ins = localStorage.getItem('ins');
@@ -20,19 +19,20 @@ window.onload = function(){
 }
 
 function printValues() {
-    console.log("income=" + income);
-    console.log("mortgage=" + mortgage);
-    console.log("rent=" + rent);
+    console.log("Monthly Income: $" + income);
+    console.log("Mortgage: $" + mortgage);
+    console.log("Rent: $" + rent);
     console.log("Subscriptions:")
     for (var i = 0; i < moneyList.length; i++) {
         console.log("    " + subList[i] + ": $" + moneyList[i]);
     }
-    console.log("car payments=" + car);
-    console.log("car insurance=" + ins);
-    console.log("phone payments=" + phone);
-    console.log("internet=" + internet);
-    console.log("loans=" + loan)
+    console.log("Car Payments: $" + car);
+    console.log("Car Insurance: $" + ins);
+    console.log("Phone Payments: $" + phone);
+    console.log("Internet Cost: $" + internet);
+    console.log("Loan Payments: $" + loan);
+    console.log("Other Expenses:");
     for (var i = 0; i < expmoneyList.length; i++) {
-        console.log("    " + expenseList[i] + ": $" + expmoneyList[i])
+        console.log("    " + expenseList[i] + ": $" + expmoneyList[i]);
     }
 }
