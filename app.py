d63ad545cb4df6f04d444138b522f25dea6b9752
maathavan27 <33641294+maathavan27@users.noreply.github.com>
2021-06-13 01:43:28 -0400

    heroku init

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, redirect, url_for, request, send_file
 
 app = Flask(__name__)
 
-#when play is pressed
 @app.route("/", methods=['GET','POST'])
 def index():
     return render_template('finance.html')
